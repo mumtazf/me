1416e3affdf52d6f34fb2e694ccf11128d340a0d
Mumtaz <fatim25m@mtholyoke.edu>
2024-06-02 16:15:20 -0700

    minor update to test deployment

--- portfolio/src/Components/Intro.jsx
+++ portfolio/src/Components/Intro.jsx
@@ -8,7 +8,7 @@ const Intro = () => {
                 <h2>About Me</h2>
                 <div class = "intro_para">
                     <p>I’m a CS student with a focus in ML. As a part of my ML fellowship at MIT, <br/> I learned that I enjoy integrating ML models with products. <br/><br/>
-    As a junior in college, I enjoy tinkering with different technologies. <br/> I realize that there is a breadth of knowledge out there so I keep myself open to learning new things.
+    As a rising senior in college, I enjoy tinkering with different technologies. <br/> I realize that there is a breadth of knowledge out there so I keep myself open to learning new things.
                     I'm currently learning React and this website is a product of my learning journey.</p>
                     <img src = {laptopImage} alt = "girl in braids inside a laptop screen"/>
                 </div>
